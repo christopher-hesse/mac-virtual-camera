aea2212af7828873359f36088271814d7d524ae0
Christopher Hesse <48501609+cshesse@users.noreply.github.com>
2020-07-05 14:12:48 -0700

    simplify cyclegan

--- mac_virtual_camera/cyclegan.py
+++ mac_virtual_camera/cyclegan.py
@@ -1,3 +1,6 @@
+# from https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+# bash ./scripts/download_cyclegan_model.sh horse2zebra
+
 import argparse
 import functools
 
@@ -5,123 +8,8 @@ import cv2
 
 import torch
 import torch.nn as nn
-from torch.nn import init
 
 IMAGE_SIZE = 256
-
-
-class Identity(nn.Module):
-    def forward(self, x):
-        return x
-
-
-def get_norm_layer(norm_type='instance'):
-    """Return a normalization layer
-
-    Parameters:
-        norm_type (str) -- the name of the normalization layer: batch | instance | none
-
-    For BatchNorm, we use learnable affine parameters and track running statistics (mean/stddev).
-    For InstanceNorm, we do not use learnable affine parameters. We do not track running statistics.
-    """
-    if norm_type == 'batch':
-        norm_layer = functools.partial(nn.BatchNorm2d, affine=True, track_running_stats=True)
-    elif norm_type == 'instance':
-        norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
-    elif norm_type == 'none':
-        def norm_layer(x): return Identity()
-    else:
-        raise NotImplementedError('normalization layer [%s] is not found' % norm_type)
-    return norm_layer
-
-
-def init_weights(net, init_type='normal', init_gain=0.02):
-    """Initialize network weights.
-
-    Parameters:
-        net (network)   -- network to be initialized
-        init_type (str) -- the name of an initialization method: normal | xavier | kaiming | orthogonal
-        init_gain (float)    -- scaling factor for normal, xavier and orthogonal.
-
-    We use 'normal' in the original pix2pix and CycleGAN paper. But xavier and kaiming might
-    work better for some applications. Feel free to try yourself.
-    """
-    def init_func(m):  # define the initialization function
-        classname = m.__class__.__name__
-        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
-            if init_type == 'normal':
-                init.normal_(m.weight.data, 0.0, init_gain)
-            elif init_type == 'xavier':
-                init.xavier_normal_(m.weight.data, gain=init_gain)
-            elif init_type == 'kaiming':
-                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
-            elif init_type == 'orthogonal':
-                init.orthogonal_(m.weight.data, gain=init_gain)
-            else:
-                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
-            if hasattr(m, 'bias') and m.bias is not None:
-                init.constant_(m.bias.data, 0.0)
-        elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
-            init.normal_(m.weight.data, 1.0, init_gain)
-            init.constant_(m.bias.data, 0.0)
-
-    net.apply(init_func)  # apply the initialization function <init_func>
-
-
-def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
-    """Initialize a network: 1. register CPU/GPU device (with multi-GPU support); 2. initialize the network weights
-    Parameters:
-        net (network)      -- the network to be initialized
-        init_type (str)    -- the name of an initialization method: normal | xavier | kaiming | orthogonal
-        gain (float)       -- scaling factor for normal, xavier and orthogonal.
-        gpu_ids (int list) -- which GPUs the network runs on: e.g., 0,1,2
-
-    Return an initialized network.
-    """
-    if len(gpu_ids) > 0:
-        assert(torch.cuda.is_available())
-        net.to(gpu_ids[0])
-    init_weights(net, init_type, init_gain=init_gain)
-    return net
-
-
-def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, init_type='normal', init_gain=0.02, gpu_ids=[]):
-    """Create a generator
-
-    Parameters:
-        input_nc (int) -- the number of channels in input images
-        output_nc (int) -- the number of channels in output images
-        ngf (int) -- the number of filters in the last conv layer
-        netG (str) -- the architecture's name: resnet_9blocks | resnet_6blocks | unet_256 | unet_128
-        norm (str) -- the name of normalization layers used in the network: batch | instance | none
-        use_dropout (bool) -- if use dropout layers.
-        init_type (str)    -- the name of our initialization method.
-        init_gain (float)  -- scaling factor for normal, xavier and orthogonal.
-        gpu_ids (int list) -- which GPUs the network runs on: e.g., 0,1,2
-
-    Returns a generator
-
-    Our current implementation provides two types of generators:
-        U-Net: [unet_128] (for 128x128 input images) and [unet_256] (for 256x256 input images)
-        The original U-Net paper: https://arxiv.org/abs/1505.04597
-
-        Resnet-based generator: [resnet_6blocks] (with 6 Resnet blocks) and [resnet_9blocks] (with 9 Resnet blocks)
-        Resnet-based generator consists of several Resnet blocks between a few downsampling/upsampling operations.
-        We adapt Torch code from Justin Johnson's neural style transfer project (https://github.com/jcjohnson/fast-neural-style).
-
-
-    The generator has been initialized by <init_net>. It uses RELU for non-linearity.
-    """
-    net = None
-    norm_layer = get_norm_layer(norm_type=norm)
-
-    if netG == 'resnet_9blocks':
-        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=9)
-    elif netG == 'resnet_6blocks':
-        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=6)
-    else:
-        raise NotImplementedError('Generator model name [%s] is not recognized' % netG)
-    return init_net(net, init_type, init_gain, gpu_ids)
 
 
 class ResnetGenerator(nn.Module):
@@ -240,9 +128,8 @@ class ResnetBlock(nn.Module):
         return nn.Sequential(*conv_block)
 
     def forward(self, x):
-        """Forward function (with skip connections)"""
-        out = x + self.conv_block(x)  # add skip connections
-        return out
+        return x + self.conv_block(x)  # add skip connections
+
 
 def __patch_instance_norm_state_dict(state_dict, module, keys, i=0):
     """Fix InstanceNorm checkpoints incompatibility (prior to 0.4)"""
@@ -258,15 +145,12 @@ def __patch_instance_norm_state_dict(state_dict, module, keys, i=0):
     else:
         __patch_instance_norm_state_dict(state_dict, getattr(module, key), keys, i + 1)
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input-path", required=True)
+    parser.add_argument("--checkpoint-path", required=True)
     args = parser.parse_args()
-
-    # TODO: 
-    # do to device here instead of in sub function
-    # abstract load model
-    # simplify network creation
 
     img = cv2.imread(args.input_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -274,10 +158,13 @@ def main():
     input_hwc = torch.from_numpy(img).to(dtype=torch.float32) * 2 / 255 - 1
     input_nhwc = input_hwc.unsqueeze(dim=0)
     input_nchw = input_nhwc.permute(0, 3, 1, 2)
-    device = torch.device("cuda")
-    net = define_G(input_nc=3,output_nc=3, ngf=64, netG="resnet_9blocks", norm='instance', use_dropout=False, init_type='normal', init_gain=0.02, gpu_ids=[0])
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    state_dict = torch.load("checkpoints/horse2zebra_pretrained/latest_net_G.pth", map_location=str(device))
+    norm_layer = norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
+    net = ResnetGenerator(input_nc=3,output_nc=3, ngf=64, norm_layer=norm_layer, use_dropout=False, n_blocks=9)
+    net.to(device)
+
+    state_dict = torch.load(args.checkpoint_path, map_location=device)
     for key in list(state_dict.keys()):  # need to copy keys here because we mutate in loop
         __patch_instance_norm_state_dict(state_dict, net, key.split('.'))
     net.load_state_dict(state_dict)
